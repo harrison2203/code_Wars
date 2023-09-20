3a1e37812306d804fead0bbcdc78affe103b23a9
Harrison <harrison.alzate-montes@epitech.eu>
2023-09-20 19:19:22 +0200

    [ADD]countSmileys

---
+++ countSmileys.js
@@ -0,0 +1,23 @@
+function countSmileys(array) {
+
+    let result = 0;
+    let regexNose = /[-~]/;
+
+    for (let i = 0; i < array.length; i ++){
+
+        if (array[i].startsWith(":") || array[i].startsWith(";")){
+            if(array[i].match(regexNose)){
+                if(array[i].endsWith("D") || array[i].endsWith(")")){
+                    result ++;
+                }
+            } else {
+                if (array[i].endsWith("D") || array[i].endsWith(")")){
+                    result ++;
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
